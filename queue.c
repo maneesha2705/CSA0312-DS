15de217eaed0a33173ba9b2b0d07cc8c41c04586
maneesha2705 <112235075+maneesha2705@users.noreply.github.com>
2023-05-15 21:15:09 +0530

    Create queue.c

---
+++ queue.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include<stdlib.h>
+#define MAX 50
+void insert();
+void delete();
+void display();
+int queue_array[MAX];
+int rear = - 1;
+int front = - 1;
+int main()
+{
+int choice;
+while (1)
+{
+printf("1.Enqueue\n");
+printf("2.Dequeue \n");
+printf("3.Display all elements of queue \n");
+printf("4.Quit \n");
+printf("Enter your choice : ");
+scanf("%d", &choice);
+switch(choice)
+{
+case 1:
+insert();
+break;
+case 2:
+delete();
+break;
+case 3:
+display();
+break;
+case 4:
+exit(1);
+default:
+printf("Wrong choice \n");
+}
+}
+}
+void insert()
+{
+int item;
+if(rear == MAX - 1)
+printf("Queue Overflow \n");
+else
+{
+if(front== - 1)
+front = 0;
+printf("Insert the element in queue : ");
+scanf("%d", &item);
+rear = rear + 1;
+queue_array[rear] = item;
+}
+}
+void delete()
+{
+if(front == - 1 || front > rear)
+{
+printf("Queue Underflow \n");
+return;
+}
+else
+{
+printf("Element deleted from queue is : %d\n", queue_array[front]);
+front = front + 1;
+}
+}
+void display()
+{
+int i;
+if(front == - 1)
+printf("Queue is empty \n");
+else
+{
+printf("Queue is :\n");
+for(i = front; i <= rear; i++)
+printf("%d ", queue_array[i]);
+printf("\n");
+}
+}
+/*
+OUTPUT:
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 1
+Insert the element in queue : 34
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 1
+Insert the element in queue : 21
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 1
+Insert the element in queue : 44
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 3
+Queue is :
+34 21 44
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 2
+Element deleted from queue is : 34
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 3
+Queue is :
+21 44
+1.Enqueue
+2.Dequeue
+3.Display all elements of queue
+4.Quit
+Enter your choice : 4
+
+--------------------------------/*
